3260bc1f4a179faabbcde5ce46cc6836c0cf8350
s21974-pj <115408471+s21974-pj@users.noreply.github.com>
2024-06-29 17:52:27 +0200

    Add files via upload

---
+++ jaz-s21974-nbp/src/main/java/org/example/jazs21974nbp/JazS21974NbpApplication.java
@@ -0,0 +1,13 @@
+package org.example.jazs21974nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS21974NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS21974NbpApplication.class, args);
+    }
+
+}
---
+++ jaz-s21974-nbp/src/test/java/org/example/jazs21974nbp/JazS21974NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jazs21974nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS21974NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
